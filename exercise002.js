a586850a74c0fc91036840b0b7519c8541aa4f7f
Adrian Yates <118460049+adeyatz@users.noreply.github.com>
2023-10-24 13:01:04 +0100

    refactor: exercise 2, task 1, called fetch in async, await function

--- exercise002.js
+++ exercise002.js
@@ -9,4 +9,15 @@ const fetchData = (apiEndPoint) => {
     .catch((error) => console.log(error));
 };
 
-fetchData(jsonTypicode);
+async function fetchMyData(apiEndPoint) {
+  try {
+    const response = await fetch(apiEndPoint);
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//fetchData(jsonTypicode);
+fetchMyData(jsonTypicode);
