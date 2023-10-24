1a9ff6a3fe553e9fd9c97b08d58bfcd4cfd2ba06
Adrian Yates <118460049+adeyatz@users.noreply.github.com>
2023-10-24 13:45:26 +0100

    feat: get json from multiple Urls and display on the console

--- exercise002.js
+++ exercise002.js
@@ -1,6 +1,16 @@
 import fetch from "node-fetch";
 
-const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
+const jsonFeeds = [
+  "https://jsonplaceholder.typicode.com/todos/1",
+
+  "https://api.punkapi.com/v2/beers/random",
+
+  "https://www.boredapi.com/api/activity",
+
+  "https://official-joke-api.appspot.com/random_joke",
+
+  //  "https://bad-api.com/nothing",
+];
 
 const fetchData = (apiEndPoint) => {
   fetch(apiEndPoint)
@@ -13,11 +23,14 @@ const fetchMyData = async (apiEndPoint) => {
   try {
     const response = await fetch(apiEndPoint);
     const json = await response.json();
+    console.log(
+      `=========================================\n${apiEndPoint}\n=========================================`
+    );
+    //    console.log(response);
     console.log(json);
   } catch (error) {
     console.log(error);
   }
 };
 
-//fetchData(jsonTypicode);
-fetchMyData(jsonTypicode);
+jsonFeeds.forEach((feedUrl) => fetchMyData(feedUrl));
